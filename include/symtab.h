030be5c23b2f17c2d2375b34efca9638fc31180c
Ryan P. Daly <mathemaphysics@gmail.com>
2018-05-08 21:53:02 -0500

    Update in formatting

--- include/symtab.h
+++ include/symtab.h
@@ -6,50 +6,50 @@
 
 typedef struct symbol
 {
-        /**
-         * The name of the variable. This is the character
-         * string which is hashed to give the location of
-         * the start of the bucket in which the values lie.
-         */
-        char name[SYMBOL_MAX_NAME_LEN]; ///< The variable name
+    /**
+     * The name of the variable. This is the character
+     * string which is hashed to give the location of
+     * the start of the bucket in which the values lie.
+     */
+    char name[SYMBOL_MAX_NAME_LEN]; ///< The variable name
 
-        /**
-         * This is the size in bytes of symbol_t::name.
-         */
-        long size; ///< The number of bytes to allocate for data
+    /**
+     * This is the size in bytes of symbol_t::name.
+     */
+    long size; ///< The number of bytes to allocate for data
 
-        /**
-         * Points to the beginning of the data stored as
-         * the value of the variable in the symbol table.
-         */
-        void *data; ///< Pointer to the data
+    /**
+     * Points to the beginning of the data stored as
+     * the value of the variable in the symbol table.
+     */
+    void *data; ///< Pointer to the data
 } symbol_t;
 
 typedef struct symtab
 {
-        /**
-         * The total number of symbols allocated.
-         */
-        long size;
+    /**
+     * The total number of symbols allocated.
+     */
+    long size;
 
-        /**
-         * The total number of symbols currently occupying
-         * the symbol table.
-         */
-        long nsyms;
+    /**
+     * The total number of symbols currently occupying
+     * the symbol table.
+     */
+    long nsyms;
 
-        /**
-         * This is the pointer to the beginning of the symbol
-         * table itself.
-         */
-        symbol_t *syms; /// The start of the list of symbols
+    /**
+     * This is the pointer to the beginning of the symbol
+     * table itself.
+     */
+    symbol_t *syms; /// The start of the list of symbols
 
-        /**
-         * Each entry in symtab_t::occs contains either a 0
-         * or a 1, indicating that the space is not occupied
-         * and occupied, respectively.
-         */
-        char *occs; /// Occupancy indicator for each entry in symbol_t::syms
+    /**
+     * Each entry in symtab_t::occs contains either a 0
+     * or a 1, indicating that the space is not occupied
+     * and occupied, respectively.
+     */
+    char *occs; /// Occupancy indicator for each entry in symbol_t::syms
 } symtab_t;
 
 int symbol_init( symbol_t *, char *, int );
@@ -74,3 +74,4 @@ void symtab_print( symtab_t * );
 
 #endif
 
+// vim: tabstop=4:softtabstop=4:shiftwidth=4:expandtab:smarttab
